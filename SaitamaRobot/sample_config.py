db02b32771bdccf85ce5b138c284fe9768ec199c
Unknowngeek2008 <102126374+Unknowngeek2008@users.noreply.github.com>
2022-03-27 23:08:15 +0530

    config.py

--- SaitamaRobot/sample_config.py
+++ SaitamaRobot/sample_config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    API_ID = 17658550 # integer value, dont use ""
+    API_HASH = "f26ad6e5f83ff17bf90fd4430c517351"
+    TOKEN = "5112142945:AAFlvOjtYGDsBD2E46M5Sc6PIOHwqa0xwWU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1248951887  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "@Entrepreneur_68"
+    SUPPORT_CHAT = "Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+         -1001527426099
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001527426099
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
